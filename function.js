6087d6961b1727c2cf219eb284b54f54d4ae8365
luismadrazo <fenixsalta29@live.com.ar>
2023-01-29 17:19:14 -0300

    Primer commit

---
+++ function.js
@@ -0,0 +1,19 @@
+
+document.getElementById("boton").onclick = function(){
+    const mensaje =  document.getElementById("mensaje").value
+    console.log("Mensaje enviado a fenixsalta29@live.com.ar: ", mensaje)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
